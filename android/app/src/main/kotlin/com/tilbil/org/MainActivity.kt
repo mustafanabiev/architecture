249c0f593eeb9adeef531329d4a60bb16f97c3eb
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2022-11-25 20:42:24 +0600

    architecture

---
+++ android/app/src/main/kotlin/com/tilbil/org/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tilbil.org.tilbil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/modules/authentication/authentication.dart
@@ -0,0 +1,3 @@
+export 'logic/authentication_cubit.dart';
+export 'view/authentication_page.dart';
+export 'view/authentication_view.dart';
